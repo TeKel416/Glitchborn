8a3cf5fb25436a530e99b5b181c79b019d593a43
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2025-08-29 11:20:06 -0300

    backup
    backup caso eu faça merda instalando um novo package

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // bom usar o update para inputs e lógicas que não envolvam física
+    private void Update()
     {
-        
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    // bom para lógicas de física
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
     }
 }
